d849cb7aa3badc2dcc4628e4e43092a78648228a
Muhammad Hamza <hafizhamza2247@gmail.com>
2023-10-18 14:50:06 +0500

    class-1

--- class1/class1.py
+++ class1/class1.py
@@ -1,1 +1,15 @@
-print("Muhammad Hamza")
+# name : str = "Muhammad Hamza"
+# print(name)
+
+# name1 : list[str] = ['a', 'b', 'c', 'd', 'e', 'f']
+# print(name1)
+# print(type(name1))
+# print(id(name1))
+# print([i for i in dir(name1) if "__" not in i])
+
+
+name : tuple[str, int, float] = ('a', 234, 23435.5)
+print(name)
+print(type(name))
+print(id(name))
+print([i for i in dir(name) if "__" not in i])
